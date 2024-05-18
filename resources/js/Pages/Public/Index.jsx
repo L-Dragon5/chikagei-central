601a335fb84305fa664a962e0ccbe537dec02ac8
L-Dragon5 <lildragon555@gmail.com>
2024-05-18 00:17:35 -0400

    Initial commit

---
+++ resources/js/Pages/Public/Index.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react';
+import { useState } from 'react';
+
+import { PublicLayout } from '@/components/PublicLayout';
+
+const Index = () => {
+  return (
+    <PublicLayout title="Index">
+      Hello
+    </PublicLayout>
+  );
+};
+
+export default Index;
